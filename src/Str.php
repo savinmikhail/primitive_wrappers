931dbf7de8a2922b98b1448a92ceafc5d49f550b
Savin Mikhail <mikhail.d.savin@gmail.com>
2024-05-03 22:19:43 +0700

    feat(str): move from static to oop

--- src/Str.php
+++ src/Str.php
@@ -4,27 +4,35 @@ declare(strict_types=1);
 
 namespace Mikhail\PrimitiveWrappers;
 
+use Exception;
+
 use function strlen;
 use function mb_strlen;
 use function mb_detect_encoding;
 
+/** anything in ASCII works as in UTF-8, so we use mb_ functions everywhere */
 class Str
 {
-    public function __construct(protected string $string)
+    public function __construct(protected string $str)
     {
     }
 
-    public static function length(string $string): int
+    public function length(): int
     {
-        return strlen($string);
+        return mb_strlen($this->str);
     }
 
-    public static function isMultibyte(string $string): bool
+    /**
+     * @throws Exception
+     */
+    public function isMultibyte(): bool
     {
-        $encoding = mb_detect_encoding($string);
+        $encoding = mb_detect_encoding($this->str);
         if ($encoding === false) {
-            throw new \Exception('Failed to detect encoding');
+            throw new Exception('Failed to detect encoding');
         }
-        return strlen($string) !== mb_strlen($string, $encoding);
+        return strlen($this->str) !== mb_strlen($this->str, $encoding);
     }
+
+ 
 }
